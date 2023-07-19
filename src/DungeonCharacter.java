9f61b358e37645ecfd0b7cb183cdebd6b06cf38a
HassanBFarhat <farhathassan@rocketmail.com>
2023-07-18 22:58:13 -0700

    Commit skeleton for DungeonCharacter.java + Room.java. Also, src folder created and rooted as source

---
+++ src/DungeonCharacter.java
@@ -0,0 +1,111 @@
+public class DungeonCharacter {
+
+    // instance fields
+
+    /** . */
+    private String myCharacterName;
+    /** . */
+    private int myCharacterHealthPoints;
+    /** . */
+    private int myCharacterHitPoints;
+    /** . */
+    private int myMinimumDamageRange;
+    /** . */
+    private int myMaximumDamageRange;
+    /** . */
+    private int myAttackSpeed;
+    /** . */
+    private double myChangeToHit;
+
+    // constructor
+
+    /** . */
+    DungeonCharacter() {
+
+    }
+
+    // methods
+
+    /** . */
+    public final int attack() {
+        return 0;
+    }
+
+    /** . */
+    public final void setCharacterName() {
+
+    }
+
+    /** . */
+    public final String getCharacterName() {
+        return "";
+    }
+
+    /** . */
+    public final void setCharacterHealthPoints() {
+
+    }
+
+    /** . */
+    public final int getCharacterHealthPoints() {
+        return 0;
+    }
+
+    /** . */
+    public final void setCharacterHitPoints() {
+
+    }
+
+    /** . */
+    public final int getCharacterHitPoints() {
+        return 0;
+    }
+
+    /** . */
+    public final void setMinimumDamageRange() {
+
+    }
+
+    /** . */
+    public final int getMinimumDamageRange() {
+        return 0;
+    }
+
+    /** . */
+    public final void setMaximumDamageRange() {
+
+    }
+
+    /** . */
+    public final int getMaximumDamageRange() {
+        return 0;
+    }
+
+    /** . */
+    public final void setAttackSpeed() {
+
+    }
+
+    /** . */
+    public final int getAttackSpeed() {
+        return 0;
+    }
+
+    /** . */
+    public final void setChanceToHit() {
+
+    }
+
+    /** . */
+    public final double getChanceToHit() {
+        return 0.0;
+    }
+
+    /** . */
+    @Override
+    public final String toString() {
+        return "";
+    }
+
+
+}
