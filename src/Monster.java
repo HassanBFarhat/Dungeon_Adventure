0362863c17ef7a0085e8535f4dcb845b63730eb5
Avi <67212741+avi53@users.noreply.github.com>
2023-07-19 12:08:01 -0700

    barebones of Monster class

--- src/Monster.java
+++ src/Monster.java
@@ -1,7 +1,15 @@
 public abstract class Monster extends DungeonCharacter {
 
+    double myMonsterChanceToHeal;
+    int myMinimumHealPoints;
+    int my MaximumHealPoints;
+
     public Monster(String name, int hitPoints, int attackSpeed, double chanceHit, int maxDamage, int minDamage) {
         super(name, hitPoints, attackSpeed, chanceHit, maxDamage, minDamage )
+    }
+
+    public int heal() {
+
     }
 
     public void setMonsterChanceToHeal() {
