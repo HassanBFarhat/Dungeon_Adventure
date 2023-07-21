424d56755683ff8cf4f857900a6aaa5e1b39fd7a
Avi <67212741+avi53@users.noreply.github.com>
2023-07-21 02:24:38 -0700

    Completed Monster.java Class

--- src/Monster.java
+++ src/Monster.java
@@ -1,45 +1,57 @@
 public abstract class Monster extends DungeonCharacter {
 
+    private static final String NEW_LINE = "\n";
     double myMonsterChanceToHeal;
     int myMinimumHealPoints;
-    int my MaximumHealPoints;
+    int myMaximumHealPoints;
 
-    public Monster(String name, int hitPoints, int attackSpeed, double chanceHit, int maxDamage, int minDamage) {
-        super(name, hitPoints, attackSpeed, chanceHit, maxDamage, minDamage )
+    public Monster(String name, int healthPoints, int hitPoints, int attackSpeed, double chanceHit, int maxDamage, int minDamage) {
+        super(name, healthPoints, hitPoints, maxDamage, minDamage, attackSpeed, chanceHit);
+        setMonsterChanceToHeal(0.1);
+        setMinimumHealPoints(5);
+        setMaximumHealPoints(50);
     }
 
     public int heal() {
-
+        if (Math.random() < myMonsterChanceToHeal) {
+            int healAmount = (int) (Math.random() * (myMaximumHealPoints - myMinimumHealPoints + 1))
+                    + myMinimumHealPoints;
+            setCharacterHealthPoints(getCharacterHealthPoints() + healAmount);
+            return healAmount;
+        }
+        return 0;
     }
 
-    public void setMonsterChanceToHeal() {
-
+    public void setMonsterChanceToHeal(final double chance) {
+        myMonsterChanceToHeal = chance;
     }
 
     public double getMonsterChangeToHeal() {
-
-
+        return myMonsterChanceToHeal;
     }
 
-    public void setMinimumHealPoints() {
-
+    public void setMinimumHealPoints(int minHealPoints) {
+        myMinimumHealPoints = minHealPoints;
     }
 
     public int getMinimumHealPoints() {
-
+        return myMinimumHealPoints;
     }
 
-    public void setMaximumHealPoints() {
-
+    public void setMaximumHealPoints(final int maxHealPoints) {
+        myMinimumHealPoints = maxHealPoints;
     }
 
     public int getMaximumHealPoints() {
-
+        return myMaximumHealPoints;
     }
 
    @Override
    public String toString() {
-
+       StringBuilder sb = new StringBuilder(super.toString());
+       sb.append("Monster's Chance to Heal: " + myMonsterChanceToHeal + NEW_LINE);
+       sb.append("Monster's Minimum Heal Points: " + myMinimumHealPoints + NEW_LINE);
+       sb.append("Monster's Maximum Heal Points: " + myMaximumHealPoints + NEW_LINE);
+       return sb.toString();
    }
-
 }
