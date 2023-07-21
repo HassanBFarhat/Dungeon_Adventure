9087f22b5417d5da8dde5653ab34932c833909c6
Avi <67212741+avi53@users.noreply.github.com>
2023-07-21 03:09:13 -0700

    Dungeon.java class

---
+++ src/Dungeon.java
@@ -0,0 +1,46 @@
+
+public class Dungeon {
+
+    private Room[][] myMazeRoom;
+    private String myAdventureLocation;
+
+    public Dungeon() {
+
+    }
+
+    public void randomlyGenerateRooms() {
+
+    }
+
+    public boolean checkIfEntranceToExitIsValid() {
+
+    }
+
+    public void placeRandomContentInRoom() {
+
+    }
+
+    public DungeonCharacter placeRandomMonsterInRoom() {
+
+    }
+
+    public void setMazeRoom() {
+
+    }
+
+    public Room[][] getMyMazeRoom() {
+        return myMazeRoom;
+    }
+
+    public void setMyAdventureLocation(String myAdventureLocation) {
+        this.myAdventureLocation = myAdventureLocation;
+    }
+
+    public String getMyAdventureLocation() {
+        return myAdventureLocation;
+    }
+
+    public String toString() {
+
+    }
+}
