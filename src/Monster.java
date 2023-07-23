0b6b12ff3e8b67c02df9653565449fe7f9a96a68
Avi <67212741+avi53@users.noreply.github.com>
2023-07-22 22:58:50 -0700

    updated Monster.java Class

--- src/Monster.java
+++ src/Monster.java
@@ -7,9 +7,6 @@ public abstract class Monster extends DungeonCharacter {
 
     public Monster(String name, int healthPoints, int hitPoints, int attackSpeed, double chanceHit, int maxDamage, int minDamage) {
         super(name, healthPoints, hitPoints, maxDamage, minDamage, attackSpeed, chanceHit);
-        setMonsterChanceToHeal(0.1);
-        setMinimumHealPoints(5);
-        setMaximumHealPoints(50);
     }
 
     public int heal() {
@@ -53,5 +50,33 @@ public abstract class Monster extends DungeonCharacter {
         return super.toString() + "Monster's Chance to Heal: " + myMonsterChanceToHeal + NEW_LINE +
                 "Monster's Minimum Heal Points: " + myMinimumHealPoints + NEW_LINE +
                 "Monster's Maximum Heal Points: " + myMaximumHealPoints + NEW_LINE;
+    }
+}
+
+class Ogre extends Monster {
+    public Ogre(String name) {
+        super(name, 200, 200, 2, 0.6, 60, 30);
+        setMonsterChanceToHeal(0.1);
+        setMinimumHealPoints(30);
+        setMaximumHealPoints(60);
+    }
+}
+
+class Gremlin extends Monster {
+    public Gremlin(String name) {
+        super(name, 70, 70, 5, 0.8, 30, 15);
+        setMonsterChanceToHeal(0.4);
+        setMinimumHealPoints(20);
+        setMaximumHealPoints(40);
+
+    }
+}
+
+class Skeleton extends Monster {
+    public Skeleton(String name) {
+        super(name, 100, 100, 3, 0.8, 50, 30);
+        setMonsterChanceToHeal(0.3);
+        setMinimumHealPoints(30);
+        setMaximumHealPoints(50);
     }
 }
