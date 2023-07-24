526432441b011e2e90345be0bc81b7ba3d9be5a1
Avi <67212741+avi53@users.noreply.github.com>
2023-07-23 22:57:21 -0700

    added specialized attack methods

--- src/Monster.java
+++ src/Monster.java
@@ -38,7 +38,7 @@ public abstract class Monster extends DungeonCharacter {
     }
 
     public void setMaximumHealPoints(final int maxHealPoints) {
-        myMinimumHealPoints = maxHealPoints;
+        myMaximumHealPoints = maxHealPoints;
     }
 
     public int getMaximumHealPoints() {
@@ -54,28 +54,56 @@ public abstract class Monster extends DungeonCharacter {
 }
 
 class Ogre extends Monster {
+
+    private static final double chanceHit = 0.3;
     public Ogre(String name) {
         super(name, 200, 200, 2, 0.6, 60, 30);
         setMonsterChanceToHeal(0.1);
         setMinimumHealPoints(30);
         setMaximumHealPoints(60);
     }
+
+    public int attack(DungeonCharacter monster) {
+        if (Math.random() <= chanceHit) {
+            return (int)((Math.random()) * (60 - 30 + 1));
+        }
+        return 0;
+    }
 }
 
 class Gremlin extends Monster {
+
+    private static final double chanceHit = 0.3;
+
     public Gremlin(String name) {
         super(name, 70, 70, 5, 0.8, 30, 15);
         setMonsterChanceToHeal(0.4);
         setMinimumHealPoints(20);
         setMaximumHealPoints(40);
     }
+
+    public int attack(DungeonCharacter monster) {
+        if (Math.random() <= chanceHit) {
+            return (int)((Math.random()) * (30 - 15 + 1));
+        }
+        return 0;
+    }
 }
 
 class Skeleton extends Monster {
+    private static final double chanceHit = 0.3;
+
     public Skeleton(String name) {
         super(name, 100, 100, 3, 0.8, 50, 30);
         setMonsterChanceToHeal(0.3);
         setMinimumHealPoints(30);
         setMaximumHealPoints(50);
+    }
+
+    public int attack(DungeonCharacter monster) {
+        if (Math.random() <= chanceHit) {
+            return (int)((Math.random()) * (50 - 30 + 1));
+        }
+        return 0;
     }
 }
