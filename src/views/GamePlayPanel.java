c1ec0c88dfe01ca017d03d664fbd202a51f06c99
HassanBFarhat <farhathassan@rocketmail.com>
2023-07-29 18:49:27 -0700

    Added instance fields and instantiations for those fields in GamePlayPanel.java class.

--- src/views/GamePlayPanel.java
+++ src/views/GamePlayPanel.java
@@ -10,18 +10,55 @@ public class GamePlayPanel extends JPanel {
 
     // instance fields
 
+    /** . */
+    private JPanel myMiniMap;
+    /** . */
+    private JPanel myCharacterAndMovementOptions;
+    /** . */
+    private JLabel myHeroJPGLabel;
+    /** . */
+    private JButton myNorthBtn;
+    /** . */
+    private JButton mySouthBtn;
+    /** . */
+    private JButton myEastBtn;
+    /** . */
+    private JButton myWestBtn;
+    /** . */
+    private JProgressBar myCharactersHealth;
+    /** . */
+    private JButton myInventoryBtn;
+    /** . */
+    private JButton mySaveGameBtn;
 
 
     // constructor
 
     /** . */
     public GamePlayPanel() {
-
+        instantiateInstanceDataFields();
     }
 
 
     // methods
 
+    /** . */
+    private void instantiateInstanceDataFields() {
+        final ImageIcon heroImg = new ImageIcon("src/imgs/MainMenuBG_Image.jpg");
+        myMiniMap = new JPanel();
+        myCharacterAndMovementOptions = new JPanel();
+        myHeroJPGLabel = new JLabel(heroImg);
+        myNorthBtn = new JButton("Move North");
+        mySouthBtn = new JButton("Move South");
+        myEastBtn = new JButton("Move East");
+        myWestBtn = new JButton("Move West");
+        myCharactersHealth = new JProgressBar();
+        myInventoryBtn = new JButton("Inventory");
+        mySaveGameBtn = new JButton("Save Game");
+    }
 
+    private void addComponentsToCharacterAndMovementPanel() {
+
+    }
 
 }
