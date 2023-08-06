25beee175fdbae5a23ff2051d1134fa19ec21241
Avi <abavisetty3@gmail.com>
2023-08-05 21:30:03 -0700

    -made some changes to the monsterfactory.java

--- src/models/MonsterFactory.java
+++ src/models/MonsterFactory.java
@@ -4,6 +4,7 @@ import org.sqlite.SQLiteDataSource;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +18,7 @@ public class MonsterFactory {
 
     // instance fields
 
-    private List<Monster> monsterList;
+    private final List<Monster> monsterList;
 
     // constructor
 
@@ -43,120 +44,52 @@ public class MonsterFactory {
     public Monster createMonster(String monsterType) {
         Monster monster = null;
 
-        String query = "SELECT * FROM monsters WHERE monster_name = ";
+        String query = "SELECT * FROM monsters WHERE monster_name = '" + monsterType + "'";
 
-        if (monsterType.equals("Ogre")) {
-            query += "'Ogre'";
-            // querying DB for content
-            try (Connection conn = ds.getConnection();
-                 Statement stmt = conn.createStatement();) {
-                ResultSet rs = stmt.executeQuery(query);
-                String monsterName = null;
-                int monsterHealth = 0;
-                int monsterHitPoints = 0;
-                int monsterMinDamage = 0;
-                int monsterMaxDamage = 0;
-                int monsterAttackSpeed = 0;
-                double monsterChanceToHit = 0.0;
-                double monsterChanceToHeal = 0.0;
-                int monsterMinHealPoints = 0;
-                int monsterMaxHealPoints = 0;
-                while (rs.next()) {
-                    monsterName = rs.getString("monster_name");
-                    monsterHealth = rs.getInt("monster_health_points");
-                    monsterHitPoints = rs.getInt("monster_hit_points");
-                    monsterMinDamage = rs.getInt("monster_min_damage");
-                    monsterMaxDamage = rs.getInt("monster_max_damage");
-                    monsterAttackSpeed = rs.getInt("monster_attack_speed");
-                    monsterChanceToHit = rs.getDouble("monster_chance_to_hit");
-                    monsterChanceToHeal = rs.getDouble("monster_chance_to_heal");
-                    monsterMinHealPoints = rs.getInt("monster_min_heal_points");
-                    monsterMaxHealPoints = rs.getInt("monster_max_heal_points");
+
+        // querying DB for content
+        try (Connection conn = ds.getConnection();
+             Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                String monsterName = rs.getString("monster_name");
+                int monsterHealth = rs.getInt("monster_health_points");
+                int monsterHitPoints = rs.getInt("monster_hit_points");
+                int monsterMinDamage = rs.getInt("monster_min_damage");
+                int monsterMaxDamage = rs.getInt("monster_max_damage");
+                int monsterAttackSpeed = rs.getInt("monster_attack_speed");
+                double monsterChanceToHit = rs.getDouble("monster_chance_to_hit");
+                double monsterChanceToHeal = rs.getDouble("monster_chance_to_heal");
+                int monsterMinHealPoints = rs.getInt("monster_min_heal_points");
+                int monsterMaxHealPoints = rs.getInt("monster_max_heal_points");
+
+                switch (monsterType) {
+                    case "Ogre":
+                        monster = new Ogre(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
+                        monster.setMonsterChanceToHeal(monsterChanceToHeal);
+                        monster.setMinimumHealPoints(monsterMinHealPoints);
+                        monster.setMaximumHealPoints(monsterMaxHealPoints);
+
+                    case "Gremlin":
+                        monster = new Gremlin(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
+                        monster.setMonsterChanceToHeal(monsterChanceToHeal);
+                        monster.setMinimumHealPoints(monsterMinHealPoints);
+                        monster.setMaximumHealPoints(monsterMaxHealPoints);
+
+                    case "Skeleton":
+                        monster = new Skeleton(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
+                        monster.setMonsterChanceToHeal(monsterChanceToHeal);
+                        monster.setMinimumHealPoints(monsterMinHealPoints);
+                        monster.setMaximumHealPoints(monsterMaxHealPoints);
                 }
-                monster = new Ogre(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
-                monster.setMonsterChanceToHeal(monsterChanceToHeal);
-                monster.setMinimumHealPoints(monsterMinHealPoints);
-                monster.setMaximumHealPoints(monsterMaxHealPoints);
-
-            } catch (final Exception e) {
-                e.printStackTrace();
-                System.exit(0);
-            }
-        } else if (monsterType.equals("Gremlin")) {
-            query += "'Gremlin'";
-            // querying DB for content
-            try (Connection conn = ds.getConnection();
-                 Statement stmt = conn.createStatement();) {
-                ResultSet rs = stmt.executeQuery(query);
-                String monsterName = null;
-                int monsterHealth = 0;
-                int monsterHitPoints = 0;
-                int monsterMinDamage = 0;
-                int monsterMaxDamage = 0;
-                int monsterAttackSpeed = 0;
-                double monsterChanceToHit = 0.0;
-                double monsterChanceToHeal = 0.0;
-                int monsterMinHealPoints = 0;
-                int monsterMaxHealPoints = 0;
-                while (rs.next()) {
-                    monsterName = rs.getString("monster_name");
-                    monsterHealth = rs.getInt("monster_health_points");
-                    monsterHitPoints = rs.getInt("monster_hit_points");
-                    monsterMinDamage = rs.getInt("monster_min_damage");
-                    monsterMaxDamage = rs.getInt("monster_max_damage");
-                    monsterAttackSpeed = rs.getInt("monster_attack_speed");
-                    monsterChanceToHit = rs.getDouble("monster_chance_to_hit");
-                    monsterChanceToHeal = rs.getDouble("monster_chance_to_heal");
-                    monsterMinHealPoints = rs.getInt("monster_min_heal_points");
-                    monsterMaxHealPoints = rs.getInt("monster_max_heal_points");
-                }
-                monster = new Ogre(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
-                monster.setMonsterChanceToHeal(monsterChanceToHeal);
-                monster.setMinimumHealPoints(monsterMinHealPoints);
-                monster.setMaximumHealPoints(monsterMaxHealPoints);
-
-            } catch (final Exception e) {
-                e.printStackTrace();
-                System.exit(0);
-            }
-        } else if (monsterType.equals("Skeleton")) {
-            query += "'Skeleton'";
-            // querying DB for content
-            try (Connection conn = ds.getConnection();
-                 Statement stmt = conn.createStatement();) {
-                ResultSet rs = stmt.executeQuery(query);
-                String monsterName = null;
-                int monsterHealth = 0;
-                int monsterHitPoints = 0;
-                int monsterMinDamage = 0;
-                int monsterMaxDamage = 0;
-                int monsterAttackSpeed = 0;
-                double monsterChanceToHit = 0.0;
-                double monsterChanceToHeal = 0.0;
-                int monsterMinHealPoints = 0;
-                int monsterMaxHealPoints = 0;
-                while (rs.next()) {
-                    monsterName = rs.getString("monster_name");
-                    monsterHealth = rs.getInt("monster_health_points");
-                    monsterHitPoints = rs.getInt("monster_hit_points");
-                    monsterMinDamage = rs.getInt("monster_min_damage");
-                    monsterMaxDamage = rs.getInt("monster_max_damage");
-                    monsterAttackSpeed = rs.getInt("monster_attack_speed");
-                    monsterChanceToHit = rs.getDouble("monster_chance_to_hit");
-                    monsterChanceToHeal = rs.getDouble("monster_chance_to_heal");
-                    monsterMinHealPoints = rs.getInt("monster_min_heal_points");
-                    monsterMaxHealPoints = rs.getInt("monster_max_heal_points");
-                }
-                monster = new Ogre(monsterName, monsterHealth, monsterHitPoints, monsterAttackSpeed, monsterChanceToHit, monsterMaxDamage, monsterMinDamage);
-                monster.setMonsterChanceToHeal(monsterChanceToHeal);
-                monster.setMinimumHealPoints(monsterMinHealPoints);
-                monster.setMaximumHealPoints(monsterMaxHealPoints);
-
-            } catch (final Exception e) {
-                e.printStackTrace();
-                System.exit(0);
             }
         }
+        catch (final SQLException e) {
+            e.printStackTrace();
+            System.exit(0);
+            throw new RuntimeException(e);
+        }
+
         return monster;
     }
 
