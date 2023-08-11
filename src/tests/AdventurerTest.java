a2ee924e4869113622644b9fa644420199ac385e
slucier-benson <104026352+slucier-benson@users.noreply.github.com>
2023-08-11 15:42:43 -0700

    Worked on 'testAddPillarToMyPillarsArray'test case in AdventurerTest.java

--- src/tests/AdventurerTest.java
+++ src/tests/AdventurerTest.java
@@ -55,49 +55,49 @@ public class AdventurerTest {
 
         // Check if the list now contains EXIT
         assertTrue(adventurer.getMyPillars().contains(RoomItems.EXIT));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(2, adventurer.getMyPillars().size());
 
         // Add HEALING_POTION and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.HEALING_POTION);
 
         // Check if the list now contains HEALING_POTION
         assertTrue(adventurer.getMyPillars().contains(RoomItems.HEALING_POTION));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(3, adventurer.getMyPillars().size());
 
         // Add PIT and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.PIT);
 
         // Check if the list now contains PIT
         assertTrue(adventurer.getMyPillars().contains(RoomItems.PIT));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(4, adventurer.getMyPillars().size());
 
         // Add ABSTRACTION_PILLAR and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.ABSTRACTION_PILLAR);
 
         // Check if the list now contains ABSTRACTION_PILLAR
         assertTrue(adventurer.getMyPillars().contains(RoomItems.ABSTRACTION_PILLAR));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(5, adventurer.getMyPillars().size());
 
         // Add ENCAPSULATION_PILLAR and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.ENCAPSULATION_PILLAR);
 
         // Check if the list now contains ENCAPSULATION_PILLAR
         assertTrue(adventurer.getMyPillars().contains(RoomItems.ENCAPSULATION_PILLAR));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(6, adventurer.getMyPillars().size());
 
         // Add INHERITANCE_PILLAR and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.INHERITANCE_PILLAR);
 
         // Check if the list now contains INHERITANCE_PILLAR
         assertTrue(adventurer.getMyPillars().contains(RoomItems.INHERITANCE_PILLAR));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(7, adventurer.getMyPillars().size());
 
         // Add POLYMORPHISM_PILLAR and check the size
         adventurer.addPillarToMyPillarsArray(RoomItems.POLYMORPHISM_PILLAR);
 
         // Check if the list now contains POLYMORPHISM_PILLAR
         assertTrue(adventurer.getMyPillars().contains(RoomItems.POLYMORPHISM_PILLAR));
-        assertEquals(1, adventurer.getMyPillars().size());
+        assertEquals(8, adventurer.getMyPillars().size());
     }
 
     @Test
