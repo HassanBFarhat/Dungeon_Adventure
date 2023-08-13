f2977062221410ed3eb4c536f229bf570281086a
Avi <abavisetty3@gmail.com>
2023-08-13 01:16:09 -0700

    -added music

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -2,14 +2,29 @@ package controller;
 
 import views.MainFrame;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public final class Main {
 
     private Main() {
         // Utility class shouldn't have public or default constructor.
     }
 
-    public static void main(final String[] theArgs) {
+    public static void main(final String[] theArgs) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         new MainFrame();
+        music();
+    }
+
+    public static void music() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        File soundfile = new File("src/controller/track001.wav");
+        AudioInputStream ais = AudioSystem.getAudioInputStream(soundfile);
+        Clip clip = AudioSystem.getClip();
+        clip.open(ais);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.start();
     }
 
 }
